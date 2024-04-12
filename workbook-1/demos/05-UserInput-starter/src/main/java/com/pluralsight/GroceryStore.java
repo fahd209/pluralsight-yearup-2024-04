64250f08aafc8c04dda85183ee3a4686ec7fcbc3
Fahd <fahdalamari2005@gmail.com>
2024-04-12 15:26:42 -0700

    UserInput starter demo

--- workbook-1/demos/05-UserInput-starter/src/main/java/com/pluralsight/GroceryStore.java
+++ workbook-1/demos/05-UserInput-starter/src/main/java/com/pluralsight/GroceryStore.java
@@ -4,48 +4,57 @@ import java.util.Scanner;
 
 public class GroceryStore
 {
+    private static Scanner userInput = new Scanner(System.in);
+    static final String PRODUCT_CHOCOLATE = "Milka Aplenmilch";
+    static final double PRODUCT_CHOCOLATE_PRICE = 2.99;
+    static final String PRODUCT_CANDY = "Skittles";
+    static final double PRODUCT_CANDY_PRICE = 1.49;
+
     public static void main(String[] args)
     {
-        Scanner userInput = new Scanner(System.in);
 
         // declare constants
-        final String PRODUCT_CHOCOLATE = "Milka Aplenmilch";
-        final double PRODUCT_CHOCOLATE_PRICE = 2.99;
-        final String PRODUCT_CANDY = "Skittles";
-        final double PRODUCT_CANDY_PRICE = 1.49;
 
         // input variables
         String customerName;
         int quantity;
+        int productId;
         double cashReceived;
 
         // calculated
-        double totalPrice;
+        double totalPrice = 0;
         double changeReturned;
 
+        // get username
+        customerName = getName();
 
-        // get user input
-        System.out.print("Please enter your name: ");
-        customerName = userInput.nextLine(); // waits for the user to enter their name
+        //display product
+        displayProducts(customerName);
 
-        System.out.println();
-        System.out.printf("Welcome %s. Today's products are: \n", customerName);
-        System.out.printf("A) %s - $ %.2f \n", PRODUCT_CHOCOLATE, PRODUCT_CHOCOLATE_PRICE);
-        System.out.printf("B) %s - $ %.2f \n", PRODUCT_CANDY, PRODUCT_CANDY_PRICE);
-        System.out.println();
+        // getting product id
+        productId = getSelectedProductId();
 
-        System.out.print("How many would you like to buy? ");
-        quantity = userInput.nextInt();
+        // getting selected number of quantity
+        quantity = getSelectedQuantity();
 
-        // calculations
-        totalPrice = PRODUCT_CHOCOLATE_PRICE * quantity;
+        if(productId == 1)
+        {
+            totalPrice = PRODUCT_CHOCOLATE_PRICE * quantity;
+        }
+        else if(productId == 2)
+        {
+            totalPrice = PRODUCT_CANDY_PRICE * quantity;
+        }
+        else
+        {
+            System.out.println("Wrong number please choose between (1-2)");
+        }
 
-        System.out.printf("The total is %.2f \n", totalPrice);
+        // display total price and display total
+        cashReceived = getCashAndDisplayTotal(totalPrice);
 
-        System.out.print("Please enter your cash amount: ");
-        cashReceived = userInput.nextInt();
-
-        changeReturned = cashReceived - totalPrice;
+        //gets total and displays it
+        changeReturned = getChange(totalPrice, cashReceived);
 
         // display the receipt
         System.out.println();
@@ -53,5 +62,50 @@ public class GroceryStore
         System.out.printf("Total:  $ %6.2f \n", totalPrice);
         System.out.printf("Cash:   $ %6.2f \n", cashReceived);
         System.out.printf("Change: $ %6.2f \n", changeReturned);
+    }
+
+    public static String getName()
+    {
+        // get user input
+        System.out.print("Please enter your name: ");
+        String customerName = userInput.nextLine(); // waits for the user to enter their name
+        return customerName;
+    }
+
+    public static void displayProducts(String name)
+    {
+        System.out.println();
+        System.out.printf("Welcome %s. Today's products are: \n", name);
+        System.out.printf("1) %s - $ %.2f \n", PRODUCT_CHOCOLATE, PRODUCT_CHOCOLATE_PRICE);
+        System.out.printf("2) %s - $ %.2f \n", PRODUCT_CANDY, PRODUCT_CANDY_PRICE);
+        System.out.println();
+    }
+
+    public static int getSelectedProductId(){
+        System.out.println("Please select a product: ");
+        int productId = userInput.nextInt();
+        return productId;
+    }
+
+    public static int getSelectedQuantity()
+    {
+        System.out.print("How many would you like to buy? ");
+        int quantity = userInput.nextInt();
+        return quantity;
+    }
+
+    public static double getCashAndDisplayTotal(double total){
+        System.out.printf("The total is %.2f \n", total);
+
+        System.out.print("Please enter your cash amount: ");
+        double cashReceived = userInput.nextInt();
+        return cashReceived;
+    }
+
+    public static double getChange(double total, double cashReceived)
+    {
+
+        double changeReturned = cashReceived - total;
+        return changeReturned;
     }
 }
